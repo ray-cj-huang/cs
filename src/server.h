2e041c58933ac01f9532d5b80fbfc35bcbaedd44
Bryan Wong <bryanjwong@g.ucla.edu>
2022-04-07 23:55:09 +0000

    #What Refactors server_main.cc into source and header file for each class (session and server)
    #Why As per assignment instructions and for increased modularity.
    
    #How Files moved into session.h, session.cc, server.h, server.cc. CMakeLists modified to link new files.
    
    #Testing From an eye test, echo server still works as intending after building with changes.
    
    Change-Id: I08a3fe4c759ee599d04fce9a148771e85218b17c

---
+++ src/server.h
@@ -0,0 +1,23 @@
+#ifndef server_h
+#define server_h
+
+#include <boost/asio.hpp>
+#include "session.h"
+
+using boost::asio::ip::tcp;
+
+class server
+{
+public:
+  server(boost::asio::io_service& io_service, short port);
+
+private:
+  void start_accept();
+  void handle_accept(session* new_session,
+      const boost::system::error_code& error);
+
+  boost::asio::io_service& io_service_;
+  tcp::acceptor acceptor_;
+};
+
+#endif
